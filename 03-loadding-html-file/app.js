d66a13680df9daec06e296d9f7f8c743efcaf3fd
Rohit Ingle <launch.json>
2025-11-27 16:49:28 +0530

    html file  rendering adn 404 page

---
+++ 03-loadding-html-file/app.js
@@ -0,0 +1,20 @@
+// loadding or rendering html file in express js
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    const homeHtmlFile = path.resolve('./home.html');
+    res.sendFile(homeHtmlFile);
+})
+
+app.get('/about',(req,res)=>{
+    const aboutHtmlFile = path.resolve('./about.html');
+    res.sendFile(aboutHtmlFile);
+})
+app.use((req,res)=>{
+     const errorHtmlFile = path.resolve('./404.html');
+    res.status(404).sendFile(errorHtmlFile);
+})
+app.listen(4000);
